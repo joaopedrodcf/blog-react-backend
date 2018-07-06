17a53b99e8fcf22e47bef8c02f01523738a05a47
joaopedrodcf <joaoferr93@gmail.com>
2018-07-06 01:24:50 +0100

    Add send image to cloudinary without creating it locally

--- app/routes/cloudinaryRoutes.js
+++ app/routes/cloudinaryRoutes.js
@@ -15,9 +15,6 @@ module.exports = app => {
   });
 
   const storage = multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, './my-uploads');
-    },
     filename(req, file, cb) {
       cb(null, `${file.fieldname}-${Date.now()}.png`);
     }
@@ -26,9 +23,6 @@ module.exports = app => {
   const upload = multer({ storage });
 
   app.post('/api/upload', upload.single('image'), (req, res) => {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-
     cloudinary.uploader.upload(req.file.path, result => {
       console.log(result);
     });
